167dd9ff8227d60f6c91d4ca01a100775e73aca6
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2023-12-21 03:28:24 -0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import examples
-
-if __name__ == '__main__':
-    examples.read_markets_test()
-    examples.write_create_market_order_test('my_wallet_address',
-                                            'my_private_key')
